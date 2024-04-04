82da1afed3dbb2b1fe02fab44df19144028da025
NjugunaW <njugunawangui10@gmail.com>
2024-04-05 01:42:47 +0300

    Correction Version2

--- pythonProject/myproject/triviabyte/urls.py
+++ pythonProject/myproject/triviabyte/urls.py
@@ -5,7 +5,7 @@ from .views import get_quiz
 urlpatterns = [
     path('', views.triviabyte_view, name='triviabyte_view'),
     path('api/quiz/', views.get_quiz, name='get_quiz'),
-    path('submit_answers/', views.submit_answers, name='submit_answers')
+    path('submit_answers/', views.submit_answers, name='submit_answers'),
     path('music.html', views.music_view, name='music'),
     path('science.html', views.science_view, name='science'),
     path('quiz.html', views.quiz_view, name='quiz'),
